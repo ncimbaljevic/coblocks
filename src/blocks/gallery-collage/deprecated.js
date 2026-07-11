86c34194dd8cbef16db3b146574b4a7994a23146
Nemanja Cimbaljevic <zion.trooper@gmail.com>
2026-07-11 03:20:34 +0200

    Migrate blocks-4 bundle to Block API v3 (click-to-tweet, food-and-drinks, gallery-collage)
    Part 4 of the Block API v3 migration, grouped to match the blocks-4.js build
    bundle. Each migrated block keeps a deprecation for byte-identical backward
    compatibility with existing content.

--- src/blocks/gallery-collage/deprecated.js
+++ src/blocks/gallery-collage/deprecated.js
@@ -21,6 +21,97 @@ const deprecated =
 [ {
 	attributes: {
 		...GalleryAttributes,
+		...metadata.attributes,
+		gutter: {
+			type: 'string',
+			default: 'small',
+		},
+	},
+	save( { attributes } ) {
+		const {
+			animation,
+			captions,
+			captionStyle,
+			filter,
+			images,
+			lightbox,
+			linkTo,
+			rel,
+			shadow,
+			target,
+		} = attributes;
+
+		const classes = classnames( 'wp-block-coblocks-gallery-collage__figure', {
+			[ `shadow-${ shadow }` ]: shadow && shadow !== 'none',
+		} );
+
+		return (
+			<GutterWrapper { ...attributes }>
+				<div aria-label={ __( `Collage Gallery`, 'coblocks' ) }
+					className={ classnames( {
+						[ `has-filter-${ filter }` ]: filter !== 'none',
+						[ `has-caption-style-${ captionStyle }` ]: captions && captionStyle !== undefined,
+						'has-lightbox': lightbox,
+					} ) }>
+					<ul>
+						{ images.sort( ( a, b ) => parseInt( a.index ) - parseInt( b.index ) )
+							// Limit images output based on he selector style.
+							.filter( ( image ) => parseInt( image.index ) < ( [ 'is-style-tiled', 'is-style-layered' ].includes( attributes.className ) ? 4 : 5 ) )
+							.map( ( image, index ) => {
+								let href;
+
+								switch ( linkTo ) {
+									case 'media':
+										href = image.url;
+										break;
+									case 'attachment':
+										href = image.link;
+										break;
+								}
+
+								// If an image has a custom link, override the linkTo selection.
+								if ( image.imgLink ) {
+									href = image.imgLink;
+								}
+
+								const itemClasses = classnames(
+									'wp-block-coblocks-gallery-collage__item',
+									`item-${ index + 1 }`,
+									{
+										[ `coblocks-animate` ]: animation,
+									}
+								);
+								const imgClasses = classnames( image.id && [ `wp-image-${ image.id }` ] );
+								// eslint-disable-next-line jsx-a11y/img-redundant-alt
+								const img = typeof image.url === 'undefined' ? null : ( <img alt="Gallery Image" className={ imgClasses } data-id={ image.id } data-imglink={ image.imgLink } data-index={ image.index } data-link={ image.link } src={ image.url } /> );
+
+								return (
+									<li
+										className={ itemClasses }
+										data-coblocks-animation={ animation }
+										key={ `image-${ index }` }
+									>
+										{ img &&
+										<figure className={ classes }>
+											{ href ? <a href={ href } rel={ rel } target={ target }>{ img }</a> : img }
+											{ captions && image.caption && !! image.caption.length && (
+												<RichText.Content className="wp-block-coblocks-gallery-collage__caption" tagName="figcaption" value={ image.caption } />
+											) }
+										</figure>
+										}
+									</li>
+								);
+							} )
+						}
+					</ul>
+				</div>
+			</GutterWrapper>
+		);
+	},
+},
+{
+	attributes: {
+		...GalleryAttributes,
 		...BackgroundAttributes,
 		...metadata.attributes,
 		gutter: {
